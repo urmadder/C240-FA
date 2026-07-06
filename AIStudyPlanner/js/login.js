744c3b2192a76f5a127e91a2e1fa5d1f8c8acd66
cheesecake390 <24030994@myrp.edu.sg>
2026-07-06 19:10:34 +0800

    fix

--- AIStudyPlanner/js/login.js
+++ AIStudyPlanner/js/login.js
@@ -1,71 +1,56 @@
 const CLIENT_ID = "529281795879-6g91qb73fpo1527f4cap748r3aq4nq1n.apps.googleusercontent.com";
 
+let tokenClient;
+
 window.onload = () => {
 
-    google.accounts.oauth2.initTokenClient({
+    tokenClient = google.accounts.oauth2.initTokenClient({
         client_id: CLIENT_ID,
         scope: "openid email profile",
-        callback: async (response) => {
-
-            if (response.error) {
-                console.error(response);
-                return;
-            }
-
-            // Get the user's profile
-            const res = await fetch(
-                "https://www.googleapis.com/oauth2/v3/userinfo",
-                {
-                    headers: {
-                        Authorization: `Bearer ${response.access_token}`
-                    }
-                }
-            );
-
-            const user = await res.json();
-
-            console.log(user);
-
-            // Save user details
-            localStorage.setItem("userName", user.name);
-            localStorage.setItem("userEmail", user.email);
-            localStorage.setItem("userPicture", user.picture);
-
-            // Redirect
-            window.location.href = "dashboard.html";
-
-        }
+        callback: handleGoogleLogin
     });
 
-    document.getElementById("googleSignInBtn").onclick = () => {
+    document
+        .getElementById("googleSignInBtn")
+        .addEventListener("click", () => {
 
-        google.accounts.oauth2.initTokenClient({
-            client_id: CLIENT_ID,
-            scope: "openid email profile",
-            callback: async (response) => {
+            tokenClient.requestAccessToken();
 
-                if (response.error) return;
-
-                const res = await fetch(
-                    "https://www.googleapis.com/oauth2/v3/userinfo",
-                    {
-                        headers: {
-                            Authorization: `Bearer ${response.access_token}`
-                        }
-                    }
-                );
-
-                const user = await res.json();
-
-                localStorage.setItem("userName", user.name);
-                localStorage.setItem("userEmail", user.email);
-                localStorage.setItem("userPicture", user.picture);
-
-                window.location.href = "dashboard.html";
-
-            }
-        }).requestAccessToken();
-
-    };
+        });
 
 };
+
+async function handleGoogleLogin(response) {
+
+    if (response.error) {
+        console.error(response);
+        return;
+    }
+
+    try {
+
+        const res = await fetch(
+            "https://www.googleapis.com/oauth2/v3/userinfo",
+            {
+                headers: {
+                    Authorization: `Bearer ${response.access_token}`
+                }
+            }
+        );
+
+        const user = await res.json();
+
+        console.log(user);
+
+        localStorage.setItem("user", JSON.stringify(user));
+
+        window.location.href = "dashboard.html";
+
+    }
+    catch (err) {
+
+        console.error(err);
+
+    }
+
+}
