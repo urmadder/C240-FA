c15af66fb8c5ddb5b00356110e11350c7b60abb8
cheesecake390 <24030994@myrp.edu.sg>
2026-07-07 10:47:44 +0800

    Firebase

--- AIStudyPlanner/js/firebase.js
+++ AIStudyPlanner/js/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBelXBkPhha_pwCA6Qsix2BRdJDDU1q6mM",
+  apiKey: "YOUR_API_KEY",
   authDomain: "ai-study-planner-1e39d.firebaseapp.com",
   projectId: "ai-study-planner-1e39d",
   storageBucket: "ai-study-planner-1e39d.firebasestorage.app",
@@ -13,17 +11,9 @@ const firebaseConfig = {
   appId: "1:526657588982:web:8a08b546f86610bc3e66d7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
-
-// Initialize Authentication
 const auth = getAuth(app);
-
-// Google Sign-In Provider
 const provider = new GoogleAuthProvider();
 
-// Export everything
 export { app, db, auth, provider };
